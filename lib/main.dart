b7f962bd4470b64e6792347880fb2696e262accf
2022508 <c2022508@hallam.shu.ac.uk>
2024-04-28 17:49:18 +0100

    notifications implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:fitness_app/services/auth_page.dart';
+import 'package:fitness_app/services/notifications_services.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final permissionGranted = await NotificationServices.checkPermission();
+  if (permissionGranted) {
+    await NotificationServices.initialiseNotification();
+    await NotificationServices.cancelNotification();
+    await NotificationServices.displayNotification(
+      title: 'Fitness App',
+      body: "We haven't seen you in a while",
+      scheduled: true,
+      interval: 604800, // 1 week in seconds
+    );
+  }
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
