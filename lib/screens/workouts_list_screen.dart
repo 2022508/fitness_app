be83cd850379db54ea0d89d2504afd201bc462d8
2022508 <c2022508@hallam.shu.ac.uk>
2024-05-01 23:27:33 +0100

    animations

--- lib/screens/workouts_list_screen.dart
+++ lib/screens/workouts_list_screen.dart
@@ -47,12 +47,30 @@ class WorkoutListScreen extends StatelessWidget {
                                     onPressed: () {
                                       Navigator.push(
                                         context,
-                                        MaterialPageRoute(
-                                          builder: (context) =>
+                                        PageRouteBuilder(
+                                          pageBuilder: (_, __, ___) =>
                                               ViewWorkoutsScreen(
                                             workoutName: docIDs[index],
                                             database: db,
                                           ),
+                                          transitionsBuilder: (context,
+                                              animation,
+                                              secondaryAnimation,
+                                              child) {
+                                            const begin = Offset(1.0, 0.0);
+                                            const end = Offset.zero;
+                                            const curve = Curves.ease;
+
+                                            var tween = Tween(
+                                                    begin: begin, end: end)
+                                                .chain(
+                                                    CurveTween(curve: curve));
+
+                                            return SlideTransition(
+                                              position: animation.drive(tween),
+                                              child: child,
+                                            );
+                                          },
                                         ),
                                       );
                                     },
